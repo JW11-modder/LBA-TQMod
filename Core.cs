b829f04ac7d58a5da554ddfa7905d7b18262f8ef
JW Modder <jade.wayfarer@gmail.com>
2026-07-01 00:35:22 +0300

    1.0.0

---
+++ Core.cs
@@ -0,0 +1,177 @@
+﻿using HarmonyLib;
+using JModder;
+using MelonLoader;
+using MelonLoader.Preferences;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(LBAMod.Core), "LBAMod", "1.0.0", "jw11-modder", null)]
+[assembly: MelonGame("2point21", "LittleBigAdventureTwinsensQuest")]
+
+namespace LBAMod
+{
+    public class Core : MelonMod
+    {
+        public static Core Instance { get; private set; }
+
+        private static MelonPreferences_Category MultiplierFloatCategory;
+        private static MelonPreferences_Category MultiplierIntCategory;
+        private static MelonPreferences_Category ToggleCategory;
+
+        private static MelonPreferences_Entry<KeyCode> configMenuToggle;
+
+        private static MelonPreferences_Entry<bool> configNoPlayerDamage;
+        private static MelonPreferences_Entry<bool> configFreeMagic;
+        private static MelonPreferences_Entry<bool> configFreeKash;
+        private static MelonPreferences_Entry<bool> configFreeFuel;
+        private static MelonPreferences_Entry<bool> configFreeKeys;
+        private static MelonPreferences_Entry<bool> configFreeClover;
+
+        private static MelonPreferences_Entry<float> configMoneyMultiplier;
+        private static MelonPreferences_Entry<float> configWeaponDamageMultiplier;
+
+        public override void OnInitializeMelon()
+        {
+            Instance = this;
+
+            MultiplierFloatCategory = MelonPreferences.CreateCategory("FloatMultipliers");
+            MultiplierIntCategory = MelonPreferences.CreateCategory("IntMultipliers");
+            ToggleCategory = MelonPreferences.CreateCategory("Toggles");
+
+            configNoPlayerDamage = ToggleCategory.CreateEntry<bool>("configNoPlayerDamage", false, "No damage to player character");
+            configFreeMagic = ToggleCategory.CreateEntry<bool>("configFreeMagic", false, "Don't spend magic");
+            configFreeKash = ToggleCategory.CreateEntry<bool>("configFreeKash", false, "Don't spend money");
+            configFreeFuel = ToggleCategory.CreateEntry<bool>("configFreeFuel", false, "Don't spend fuel");
+            //configFreeKeys = ToggleCategory.CreateEntry<bool>("configFreeKeys", false, "Don't spend keys");
+            configFreeClover = ToggleCategory.CreateEntry<bool>("configFreeClover", false, "Don't spend clovers");
+
+            configWeaponDamageMultiplier = MultiplierFloatCategory.CreateEntry<float>("configWeaponDamageMultiplier", 1f, "Player damage multiplier", validator: new ValueRange<float>(1f, 20f));
+            configMoneyMultiplier = MultiplierFloatCategory.CreateEntry<float>("configMoneyMultiplier", 1f, "Money income multiplier", validator: new ValueRange<float>(1f, 20f));
+
+            JMod.Init(Instance);
+            configMenuToggle = JMod.configMenuToggle;
+            JMod.Log("Initialized.");
+        }
+        public override void OnUpdate()
+        {
+            if (Event.current != null)
+                if (Event.current.keyCode == configMenuToggle.Value && Event.current.type == EventType.KeyDown)
+                    JMod.SwitchMenu(false);
+
+            if (Event.current != null)
+                if (Event.current.keyCode == KeyCode.Escape && Event.current.type == EventType.KeyDown)
+                    JMod.SwitchMenu(true);
+        }
+
+        public override void OnGUI()
+        {
+            JMod.ShowMenu();
+        }
+
+        // configNoPlayerDamage
+        // configWeaponDamageMultiplier
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Hit))]
+        class CharacterHitPatch1
+        {
+            static bool Prefix(ref Character hitter, ref int damages, ref Character __instance, ref MovingObject ___m_moving)
+            {
+                if (__instance.GetMoving().IsPlayer() && configNoPlayerDamage.Value)
+                {
+                    damages = 0;
+                    __instance.m_lifePoints = 50;
+                }    
+                if (hitter.GetMoving().IsPlayer() && configWeaponDamageMultiplier.Value > 1)
+                {
+                    damages = Mathf.RoundToInt(damages * configWeaponDamageMultiplier.Value);
+                }    
+                return true;
+            }
+
+        }
+
+        // configFreeMagic
+
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.UseMagicPoints))]
+        class UseMagicPointsPatch1
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if (!configFreeMagic.Value)
+                    return true;
+                __result = true;
+                return false;
+            }
+        }
+
+        // configFreeFuel
+
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.UseFuel))]
+        class UseFuelPatch1
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if (!configFreeFuel.Value)
+                    return true;
+                __result = true;
+                return false;
+            }
+        }
+
+        // configFreeKeys
+
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.UseLittleKey))]
+        class UseLittleKeyPatch1
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if (!configFreeKeys.Value)
+                    return true;
+                __result = true;
+                return false;
+            }
+        }
+
+        // configFreeClover
+
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.UseClover))]
+        class UseCloverPatch1
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if (!configFreeClover.Value)
+                    return true;
+                GameHandler.FullPoints();
+                __result = true;
+                return false;
+            }
+        }
+
+        // configMoneyMultiplier
+
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.AddKash))]
+        class AddKashPatch1
+        {
+            static bool Prefix(ref int count)
+            {
+                if (configMoneyMultiplier.Value <= 1 && count > 0)
+                    return true;
+                count = Mathf.RoundToInt(count * configMoneyMultiplier.Value);
+                return true;
+            }
+
+        }
+        [HarmonyPatch(typeof(GameHandler), nameof(GameHandler.UseKash))]
+        class UseKashPatch1
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if (!configFreeKash.Value)
+                    return true;
+                __result = true;
+                return false;
+            }
+
+        }
+    }
+}
---
+++ JMod_melon.cs
@@ -0,0 +1,406 @@
+﻿#define ENABLE_INPUT_SYSTEM
+using MelonLoader;
+using MelonLoader.Preferences;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity.IO;
+using UnityEngine.EventSystems;
+
+
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+using UnityEngine.UI;
+
+
+namespace JModder
+{
+    public class JMod : MelonMod
+    {
+        public const string GUID = "jw11-modder.JMod";
+        public const string NAME = "JMod";
+        public const string AUTHOR = "jw11-modder";
+        public const string VERSION = "1.0.2";
+        public static MelonMod Instance { get; private set; }
+        public static bool showCheatsPopup = false;
+
+        private static GUIStyle JModStyleT = new GUIStyle();
+        private static GUIStyle JModStyleH = new GUIStyle();
+        private static GUIStyle JModStyleP = new GUIStyle();
+        private static GUIStyle JModStylePV = new GUIStyle();
+        private static GUIStyle JModStyleB = new GUIStyle();
+        private static GUIStyle JModStyleS = new GUIStyle();
+        private static GUIStyle JModStyleST = new GUIStyle();
+        private static GUIStyle JModStyleBlank = new GUIStyle();
+
+        private static Color JModColor = new Color(0.0f, 0.85f, 0.85f);
+
+        private static Texture2D consoleBackground = new Texture2D(1, 1, TextureFormat.RGBAFloat, false);
+
+        private static Texture2D sliderBackground = new Texture2D(1, 1, TextureFormat.RGBAFloat, false);
+
+        private static Rect jModWindowRect;
+        private static Rect _screenRect;
+
+        private static MelonPreferences_Category ModConfCategory;
+        private static MelonPreferences_Category MultiplierFloatCategory;
+        private static MelonPreferences_Category MultiplierIntCategory;
+        private static MelonPreferences_Category ToggleCategory;
+        private static List<MelonPreferences_Category> CustomCategoryList = new List<MelonPreferences_Category>();
+
+        public static MelonPreferences_Entry<KeyCode> configMenuToggle;
+
+        private static CursorLockMode lastLockMode;
+        private static bool lastVisibleState;
+
+        public static GameObject CanvasRoot { get; private set; }
+
+        private static EventSystem jModEventSys;
+        private static EventSystem lastEventSys;
+
+
+#if ENABLE_INPUT_SYSTEM
+        public static Key KeycodeToKey(KeyCode keyCode)
+        {
+            foreach (Key key in System.Enum.GetValues(typeof(Key)))
+            {
+                if (key.ToString().ToLower() == keyCode.ToString().ToLower())
+                {
+                    return key;
+                }
+            }
+
+            LogWarning("can't find Key matching KeyCode " + keyCode.ToString());
+            return Key.None;
+        }
+#endif
+
+        public static void LogHandler(string log, LogType level)
+        {
+            switch (level)
+            {
+                case LogType.Log:
+                    Instance.LoggerInstance.Msg(log);
+                    return;
+                case LogType.Warning:
+                case LogType.Assert:
+                    Instance.LoggerInstance.Warning(log);
+                    return;
+                case LogType.Exception:
+                case LogType.Error:
+                    Instance.LoggerInstance.Error(log);
+                    return;
+            }
+        }
+        public static void Log(object message)
+            => Log(message, LogType.Log);
+
+        public static void LogWarning(object message)
+            => Log(message, LogType.Warning);
+
+        public static void LogError(object message)
+            => Log(message, LogType.Error);
+
+        internal static void Log(object message, LogType logType)
+        {
+            string log = message?.ToString() ?? "";
+
+            switch (logType)
+            {
+                case LogType.Log:
+                case LogType.Assert:
+                    Instance.LoggerInstance.Msg(log); break;
+
+                case LogType.Warning:
+                    Instance.LoggerInstance.Warning(log); break;
+
+                case LogType.Error:
+                case LogType.Exception:
+                    Instance.LoggerInstance.Error(log); break;
+            }
+        }
+
+        public static void Init(MelonMod instance)
+        {
+
+            Instance = instance;
+
+            JModStyleH.alignment = TextAnchor.MiddleCenter;
+            JModStyleH.fontSize = 20;
+            JModStyleH.fontStyle = FontStyle.Bold;
+            JModStyleH.normal.textColor = JModColor;
+
+            JModStyleP.fontSize = 16;
+            JModStyleP.normal.textColor = JModColor;
+
+            JModStylePV.fontSize = 16;
+            JModStylePV.fontStyle = FontStyle.Bold;
+            JModStylePV.normal.textColor = JModColor;
+
+            JModStyleB.alignment = TextAnchor.UpperCenter;
+            JModStyleB.fontSize = 24;
+            JModStyleB.fontStyle = FontStyle.Bold;
+            JModStyleB.normal.textColor = JModColor;
+            JModStyleB.normal.background = consoleBackground;
+
+            consoleBackground.SetPixel(0, 0, new Color(0.003f, 0.003f, 0.01f, 0.92f));
+            consoleBackground.Apply();
+
+            sliderBackground.SetPixel(0, 0, new Color(0.05f, 0.05f, 0.05f));
+            sliderBackground.Apply();
+
+            ModConfCategory = MelonPreferences.CreateCategory("JModConfiguration");
+            configMenuToggle = ModConfCategory.CreateEntry("ToggleKey", KeyCode.F7, "Main Menu Toggle Key");
+            CustomCategoryList.Clear();
+            foreach (var category in MelonPreferences.Categories)
+            {
+                switch (category.Identifier)
+                {
+                    case "FloatMultipliers":
+                        {
+                            MultiplierFloatCategory = category;
+                            Log("Float Multipliers loaded!");
+                            break;
+                        }
+                    case "IntMultipliers":
+                        {
+                            MultiplierIntCategory = category;
+                            Log("Int Multipliers loaded!");
+                            break;
+                        }
+                    case "Toggles":
+                        {
+                            ToggleCategory = category;
+                            Log("Toggles loaded!");
+                            break;
+                        }
+                    case "JModConfiguration":
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            CustomCategoryList.Add(category);
+                            Log("Custom category: " + category.DisplayName + " loaded!");
+                            break;
+                        }
+
+                }
+            }
+
+            CanvasRoot = new GameObject("JModCanvas");
+            UnityEngine.Object.DontDestroyOnLoad(CanvasRoot);
+            CanvasRoot.hideFlags |= HideFlags.HideAndDontSave;
+            CanvasRoot.layer = 5;
+            CanvasRoot.transform.position = new Vector3(0f, 0f, 1f);
+
+            CanvasRoot.SetActive(false);
+
+            jModEventSys = CanvasRoot.AddComponent<EventSystem>();
+            jModEventSys.enabled = false;
+
+            CanvasRoot.SetActive(true);
+
+            Log("JMod Init complete!");
+            Log("Menu key: " + configMenuToggle.Value.ToString());
+        }
+
+        public static bool SwitchMenu(bool isEscape)
+        {
+            if (!showCheatsPopup && !isEscape)
+            {
+                lastLockMode = Cursor.lockState;
+                lastVisibleState = Cursor.visible;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                showCheatsPopup = !showCheatsPopup;
+
+                lastEventSys = EventSystem.current;
+                lastEventSys.enabled = false;
+                jModEventSys.enabled = true;
+            }
+            else
+                if (showCheatsPopup)
+                {
+                    Cursor.lockState = lastLockMode;
+                    Cursor.visible = lastVisibleState;
+                    MelonPreferences.Save();
+                    showCheatsPopup = !showCheatsPopup;
+#if ENABLE_INPUT_SYSTEM
+                    InputSystem.ResetDevice(Mouse.current);
+#else
+                    Input.ResetInputAxes();
+#endif
+                    jModEventSys.enabled = false;
+                    lastEventSys.enabled = true;
+
+
+                }   
+            return showCheatsPopup;
+        }
+
+        public static void ShowMenu()
+        {
+            if (showCheatsPopup)
+            {
+
+                JModStyleT = GUI.skin.GetStyle("toggle");
+                JModStyleT.fontSize = 16;
+                JModStyleT.normal.textColor = new Color(0.6f, 0.6f, 0.6f);
+                JModStyleT.onNormal.textColor = JModColor;
+
+                JModStyleS = GUI.skin.GetStyle("horizontalslider");
+                JModStyleS.normal.background = sliderBackground;
+
+                JModStyleST = GUI.skin.GetStyle("horizontalsliderthumb");
+
+                jModWindowRect = new Rect(Screen.width / 2 - 425, Screen.height / 2 - 425, 850, 850);
+                _screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+                GUI.Window(0, jModWindowRect, ModMenuWindow, "MOD OPTIONS", JModStyleB);
+
+#if ENABLE_INPUT_SYSTEM
+                Vector2 mousePosition = Mouse.current.position.ReadValue();
+#else
+                Vector2 mousePosition = Input.mousePosition;
+#endif
+                mousePosition.y = Screen.height - mousePosition.y;
+
+                if (GUI.Button(_screenRect, string.Empty, JModStyleBlank) && !jModWindowRect.Contains(mousePosition))
+                {
+
+                }
+#if ENABLE_INPUT_SYSTEM
+                if (jModWindowRect.Contains(mousePosition))
+#else
+                if (jModWindowRect.Contains(mousePosition) && !Input.GetKeyDown(configMenuToggle.Value) && !Input.GetKeyUp(configMenuToggle.Value))
+#endif
+                {
+                    Input.ResetInputAxes();
+                }
+                if (jModEventSys.IsPointerOverGameObject())
+                {
+
+                }
+            }
+        }
+
+        private static void ModMenuWindow(int windowId)
+        {
+            var yAxis = 40;
+            var xAxis = 20;
+            GUI.Label(new Rect(xAxis, yAxis, 810, 20), "Toggle Mod Options", JModStyleH);
+            yAxis += 35;
+            if (ToggleCategory.Entries.Count > 0)
+                ShowBoolMenu(ref xAxis, ref yAxis, ref ToggleCategory);
+            yAxis += 10;
+            GUI.Label(new Rect(xAxis, yAxis, 810, 20), "Multiplier Mod Options", JModStyleH);
+            yAxis += 25;
+            if (MultiplierFloatCategory.Entries.Count > 0)
+                ShowFloatMenu(ref xAxis, ref yAxis, ref MultiplierFloatCategory);
+            if (MultiplierIntCategory.Entries.Count > 0)
+                ShowIntMenu(ref xAxis, ref yAxis, ref MultiplierIntCategory);
+
+            for (int i = 0; i < CustomCategoryList.Count ; i++)
+            {
+                var tmpcat = CustomCategoryList[i];
+                GUI.Label(new Rect(xAxis, yAxis, 810, 20), tmpcat.DisplayName, JModStyleH);
+                yAxis += 35;
+                switch (tmpcat.Entries[0].BoxedValue.GetType().ToString())
+                {
+                    case "System.Boolean":
+                        {
+                            ShowBoolMenu(ref xAxis, ref yAxis, ref tmpcat);
+                            CustomCategoryList[i] = tmpcat;
+                            continue;
+                        }
+                    case "System.Single":
+                        {
+                            ShowFloatMenu(ref xAxis, ref yAxis, ref tmpcat);
+                            CustomCategoryList[i] = tmpcat;
+                            continue;
+                        }
+                    case "System.Int32":
+                        {
+                            ShowIntMenu(ref xAxis, ref yAxis, ref tmpcat);
+                            CustomCategoryList[i] = tmpcat;
+                            continue;
+                        }
+                    default:
+                        {
+                            Log(tmpcat.Entries[0].BoxedValue.GetType().ToString());
+                            continue;
+                        }
+
+                }
+            }
+
+            if (GUI.Button(new Rect(325, 810, 200, 35), "Save settings and close"))
+            {
+                SwitchMenu(false);
+            }
+        }
+
+        public static void ShowBoolMenu(ref int xAxis, ref int yAxis, ref MelonPreferences_Category cat)
+        {
+            foreach (MelonPreferences_Entry<bool> toggle in cat.Entries.Cast<MelonPreferences_Entry<bool>>())
+            {
+                toggle.Value = GUI.Toggle(new Rect(xAxis, yAxis, 400, 20), toggle.Value, toggle.DisplayName, JModStyleT);
+                xAxis += 405;
+                if (xAxis > 800)
+                {
+                    xAxis = 20;
+                    yAxis += 25;
+                }
+            }
+            if (xAxis != 20)
+            {
+                xAxis = 20;
+                yAxis += 25;
+            }
+        }
+        public static void ShowFloatMenu(ref int xAxis, ref int yAxis, ref MelonPreferences_Category cat)
+        {
+            foreach (MelonPreferences_Entry<float> mult in cat.Entries.Cast<MelonPreferences_Entry<float>>())
+            {
+                string multLabel = mult.DisplayName;
+                ValueRange<float> range = (ValueRange<float>)mult.Validator;
+                multLabel += " (" + range.MinValue.ToString() + " - " + range.MaxValue.ToString() + ")";
+                GUI.Label(new Rect(xAxis, yAxis, 780, 20), multLabel, JModStyleP);
+                GUI.Label(new Rect(xAxis + 780, yAxis, 40, 20), mult.Value.ToString("0.0"), JModStylePV);
+                yAxis += 25;
+                mult.Value = GUI.HorizontalSlider(new Rect(xAxis, yAxis, 810, 20), mult.Value, range.MinValue, range.MaxValue, JModStyleS, JModStyleST);
+                yAxis += 15;
+            }
+        }
+        public static void ShowIntMenu(ref int xAxis, ref int yAxis, ref MelonPreferences_Category cat)
+        {
+            foreach (MelonPreferences_Entry<int> mult in cat.Entries.Cast<MelonPreferences_Entry<int>>())
+            {
+                string multLabel = mult.DisplayName;
+                ValueRange<int> range = (ValueRange<int>)mult.Validator;
+                multLabel += " (" + range.MinValue.ToString() + " - " + range.MaxValue.ToString() + ")";
+                GUI.Label(new Rect(xAxis, yAxis, 620, 20), multLabel, JModStyleP);
+                if (GUI.Button(new Rect(xAxis + 680, yAxis, 40, 20), " - "))
+                {
+                    if (mult.Value > range.MinValue)
+                        mult.Value -= 1;
+                }
+                GUI.Label(new Rect(xAxis + 730, yAxis, 20, 20), mult.Value.ToString(), JModStylePV);
+                if (GUI.Button(new Rect(xAxis + 750, yAxis, 40, 20), " + "))
+                {
+                    if (mult.Value < range.MaxValue)
+                        mult.Value += 1;
+                }
+                yAxis += 30;
+            }
+        }
+    }
+}
